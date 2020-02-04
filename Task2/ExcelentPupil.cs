a5dc926a13dfe3c7f60874df0eac73120c697fa4
Loarlam <l9m_pam@live.ru>
2020-02-04 15:07:44 +0300

    Add project files.

---
+++ Task2/ExcelentPupil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class ExcelentPupil: Pupil
+    {
+        public override void Study()
+        {
+
+        }
+
+        public override void Read()
+        {
+
+        }
+
+        public override void Write()
+        {
+
+        }
+
+        public override void Relax()
+        {
+
+        }
+    }
+}
---
+++ Task2/Pupil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Pupil
+    {
+        public virtual void Study()
+        {
+
+        }
+
+        public virtual void Read()
+        {
+
+        }
+
+        public virtual void Write()
+        {
+
+        }
+
+        public virtual void Relax()
+        {
+
+        }
+    }
+}
