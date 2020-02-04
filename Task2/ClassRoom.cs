40e48c457c88a3fdd8b1e5df15730ed71a61b6aa
Loarlam <messiah02@mail.ru>
2020-02-05 00:01:32 +0300

    Правки в классе Program и ClassRoom.

--- Task2/ClassRoom.cs
+++ Task2/ClassRoom.cs
@@ -4,15 +4,24 @@ namespace Task2
 {
     class ClassRoom
     {
-        readonly ExcellentPupil excellentPupil = new ExcellentPupil();
-        readonly GoodPupil goodPupil = new GoodPupil();
-        readonly BadPupil badPupil = new BadPupil();
+        ExcellentPupil excellentPupil = new ExcellentPupil();
+        GoodPupil goodPupil = new GoodPupil();
+        BadPupil badPupil = new BadPupil();
         readonly Pupil[] pupil = new Pupil[4];
+
+        int addedPupilsToList;
+        string firstFullNamePupilFromList1, secondFullNamePupilFromList2;
+
+        public int AddedPupilsToList { set { addedPupilsToList = value; } }
+        public string FirstFullNamePupilromList { set { firstFullNamePupilFromList1 = value; } }
+        public string SecondFullNamePupilFromList { set { secondFullNamePupilFromList2 = value; } }
 
         public ClassRoom(Pupil pupil1, Pupil pupil2)
         {
             pupil[0] = pupil1;
             pupil[1] = pupil2;
+            pupil[2] = ChooseFromPupilsList();
+            pupil[3] = ChooseFromPupilsList();
         }
 
         public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3)
@@ -20,6 +29,7 @@ namespace Task2
             pupil[0] = pupil1;
             pupil[1] = pupil2;
             pupil[2] = pupil3;
+            pupil[3] = ChooseFromPupilsList();
         }
 
         public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3, Pupil pupil4)
@@ -28,6 +38,17 @@ namespace Task2
             pupil[1] = pupil2;
             pupil[2] = pupil3;
             pupil[3] = pupil4;
+        }
+
+        public Pupil ChooseFromPupilsList()
+        {
+            if (pupil.Length != 4)
+            {
+                Pupil pupilFromList = new Pupil();
+                Random random = new Random();
+                return pupilFromList;
+            }
+            return null;
         }
 
         public void GetPupilInformation()
@@ -52,6 +73,7 @@ namespace Task2
                         excellentPupil.FullNameOfPupil = pupil[i].FullName;
                         excellentPupil.Study();
                         break;
+                    //Внести дополнительные case с новичками
                     default:
                         Console.WriteLine("Ученик не найдем в базе данных.");
                         break;
